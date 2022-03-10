fafbbd00b36e5f98707f7fe89eb94c89d19d66b4
Eugene Samoilov <e.e.samoilov@mail.ru>
2022-03-10 22:14:58 +0300

    add jacson

--- src/test/java/samoiloff90/github/io/parametrs/TestData.java
+++ src/test/java/samoiloff90/github/io/parametrs/TestData.java
@@ -4,5 +4,6 @@ public class ForZipFiles {
     public static String csvFile = "test.csv";
     public static String pdfFile = "test.pdf";
     public static String xlsFile = "test.xlsx";
-    public static String pathFile = "src/test/resources/test.zip";
+    public static String pathFileZip = "src/test/resources/test.zip";
+    public static String pathFileJson = "src/test/resources/user.json";
 }
